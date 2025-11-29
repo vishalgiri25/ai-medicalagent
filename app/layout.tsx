0360c62617f6575e64ea87df9dd7b6385122d521
Shivanshu <Shivanshuk186@gmail.com>
2025-11-29 23:40:06 +0530

    Remove disableTransitionOnChange prop

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,6 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="system"
             enableSystem
-            disableTransitionOnChange
           >
             <ErrorBoundary>
               <Provider>
